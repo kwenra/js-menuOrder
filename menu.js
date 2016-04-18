8bd60f669bfd7d2ea9c19b03d87b16aa8b5188a9
kwenra <kwenra@karas-mbp.dhcp.wustl.edu>
2016-04-18 18:41:43 -0500

    lab monday 4/18

--- menu.js
+++ menu.js
@@ -4,12 +4,17 @@ var order = '';
 
 // build a function to ask questions
 
-function question() {
-  // your code goes here
+function question(orderQuestion) {
+  var answerInput = prompt(orderQuestion);
+  order += answerInput + ", ";
+  console.log(order);
 }
 
 // call the QUESTION function with the order options
-question();
+question("What kind of burger would you like?  Hamburger, Cheeseburger, Bacon burger, or Bacon cheeseburger?", order);
+question("What size would you like?  Little or Regular sized?", order);
+question("What kind of fries would you like with it?  Five Guys style, Cajun style, or no fries?", order);
+
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
